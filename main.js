0c55eea74e76cef25dd696b7344d947eb9e15644
uharshita <77626860+uharshita@users.noreply.github.com>
2021-08-13 17:16:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function preload() {
+    ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+    missed = loadSound("missed.wav");
+  }
+  
+  function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  }
