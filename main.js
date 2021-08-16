207a84ccf8e2c710b92dab9ab6a88265afe35a9a
uharshita <77626860+uharshita@users.noreply.github.com>
2021-08-16 17:28:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-function preload() {
-    ball_touch_paddel = loadSound("ball_touch_paddel.wav");
-    missed = loadSound("missed.wav");
+
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
+game_status = "";
+
+
+
+ function preload() {
+
+}
+
+function setup(){
+var canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.size(700, 600);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+
   }
-  
-  function setup(){
-  var canvas =  createCanvas(700,600);
-  canvas.parent('canvas');
-  }
+}
+function draw ()
+{
+  background(0); 
+  image(video, 0, 0, 700, 600);
+}
