63cdfea16acf5e9999cd6522723f3856b1cdc98e
uharshita <77626860+uharshita@users.noreply.github.com>
2021-08-19 08:56:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ function modelLoaded() {
   console.log('PoseNet Is Initialized');
 }
 
+
 function gotPoses(results)
 {
   if(results.length > 0)
@@ -34,11 +35,27 @@ function gotPoses(results)
 
     rightWristY = results[0].pose.rightWrist.y;
     rightWristX = results[0].pose.rightWrist.x;
-
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
   }
 }
-function draw ()
-{
+function draw(){
+
   background(0); 
   image(video, 0, 0, 700, 600);
+
+  fill("black");
+  stroke("black");
+  rect(680,0,20,700);
+
+  fill("black");
+  stroke("black");
+  rect(0,0,20,700);
+
+  if(scoreRightWrist > 0.2)
+  {
+    fill("#FF0000");
+    stroke("#FF0000");
+    circle(rightWristX, rightWristY, 30);
+  }
 }
